c9afe443559184dee2b59163efca66acfd95912b
Sean Langton <slangton1@gmail.com>
2019-01-28 20:25:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,5 +5,17 @@ class App < Sinatra::Base
   get "/" do
     "Hello World!"
   end
-  
+
+  get "/name" do
+    "My name is __"
+  end
+
+  get "/hometown" do
+    "My hometown is __"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is __"
+  end
+
 end
